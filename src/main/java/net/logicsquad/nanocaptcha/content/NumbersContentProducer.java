50fc005f03a1fd6cb9592d88559287725bf4ae48
Paul Hoadley <paulh@logicsquad.net>
2019-12-29 09:59:51 +1030

    Improves Javadocs.

--- src/main/java/net/logicsquad/nanocaptcha/content/NumbersContentProducer.java
+++ src/main/java/net/logicsquad/nanocaptcha/content/NumbersContentProducer.java
@@ -1,29 +1,47 @@
 package net.logicsquad.nanocaptcha.content;
 
-
 /**
- * TextProducer implementation that will return a series of numbers.
+ * {@link ContentProducer} implementation that will return a series of numbers.
  * 
  * @author <a href="mailto:james.childers@gmail.com">James Childers</a>
- * 
+ * @author <a href="mailto:paulh@logicsquad.net">Paul Hoadley</a>
+ * @since 1.0
  */
 public class NumbersContentProducer implements ContentProducer {
+	/**
+	 * Default length
+	 */
+	private static final int DEFAULT_LENGTH = 5;
 
-    private static final int DEFAULT_LENGTH = 5;
-    private static final char[] NUMBERS = { '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9' };
+	/**
+	 * Character set
+	 */
+	private static final char[] NUMBERS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-    private final ContentProducer _txtProd;
-    
-    public NumbersContentProducer() {
-        this(DEFAULT_LENGTH);
-    }
-    
-    public NumbersContentProducer(int length) {
-        _txtProd = new DefaultContentProducer(length, NUMBERS);
-    }
+	/**
+	 * Actual {@link ContentProducer}
+	 */
+	private final ContentProducer producer;
 
-    @Override public String getContent() {
-        return new StringBuffer(_txtProd.getContent()).toString();
-    }
+	/**
+	 * Constructor producing an object that will return content strings of
+	 * {@link #DEFAULT_LENGTH}.
+	 */
+	public NumbersContentProducer() {
+		this(DEFAULT_LENGTH);
+	}
+
+	/**
+	 * Constructor taking a {@code length} specifier.
+	 * 
+	 * @param length length of returned content strings
+	 */
+	public NumbersContentProducer(int length) {
+		producer = new DefaultContentProducer(length, NUMBERS);
+	}
+
+	@Override
+	public String getContent() {
+		return new StringBuffer(producer.getContent()).toString();
+	}
 }
