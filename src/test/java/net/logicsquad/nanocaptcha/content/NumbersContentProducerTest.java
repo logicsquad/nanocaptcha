20007cad1255a6448f1ee8cdc703925f496ce198
Paul Hoadley <paulh@logicsquad.net>
2019-12-29 10:03:08 +1030

    Adds some unit tests.

---
+++ src/test/java/net/logicsquad/nanocaptcha/content/NumbersContentProducerTest.java
@@ -0,0 +1,52 @@
+package net.logicsquad.nanocaptcha.content;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit tests on {@link NumbersContentProducer} class.
+ * 
+ * @author <a href="mailto:paulh@logicsquad.net">Paul Hoadley</a>
+ * @since 1.0
+ */
+public class NumbersContentProducerTest {
+	@Test
+	public void producerReturnsNumber() {
+		ContentProducer producer = new NumbersContentProducer();
+		for (int i = 0; i < 1000; i++) {
+			assertTrue(isNumeric(producer.getContent()));
+		}
+		return;
+	}
+
+	@Test
+	public void producerReturnsContentOfExpectedLength() {
+		for (int i = 1; i <= 1000; i++) {
+			ContentProducer producer = new NumbersContentProducer(i);
+			String content = producer.getContent();
+			assertEquals(i, content.length());
+			assertTrue(isNumeric(content));
+		}
+		return;
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void constructorThrowsOnZero() {
+		new NumbersContentProducer(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void constructorThrowsOnNegative() {
+		new NumbersContentProducer(-1);
+	}
+
+	/**
+	 * Is {@code number} a number?
+	 * 
+	 * @param number candidate number string
+	 * @return {@code true} if {@code number} is a number, otherwise {@code false}
+	 */
+	private boolean isNumeric(String number) {
+		return number.chars().allMatch(Character::isDigit);
+	}
+}
