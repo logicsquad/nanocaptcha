36e02957eda91ca0174c03a3e8887ec653fb7ad9
Paul Hoadley <paulh@logicsquad.net>
2019-12-29 10:25:57 +1030

    Adds a unit test.

---
+++ src/test/java/net/logicsquad/nanocaptcha/content/FiveLetterFirstNameContentProducerTest.java
@@ -0,0 +1,22 @@
+package net.logicsquad.nanocaptcha.content;
+
+import org.junit.Test;
+
+/**
+ * Unit tests on {@link FiveLetterFirstNameContentProducer} class.
+ * 
+ * @author <a href="mailto:paulh@logicsquad.net">Paul Hoadley</a>
+ * @since 1.0
+ */
+public class FiveLetterFirstNameContentProducerTest {
+	private ContentProducer producer = new FiveLetterFirstNameContentProducer();
+
+	// All we're doing here is making sure getContent() probably doesn't make an
+	// index calculation error.
+	@Test
+	public void getContentShouldSucceed() {
+		for (int i = 0; i < 200_000; i++) {
+			producer.getContent();
+		}
+	}
+}
