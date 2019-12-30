428a219f22d01519871edd3428b1d68ccf206294
Paul Hoadley <paulh@logicsquad.net>
2019-12-30 22:03:59 +1030

    Makes trivial PMD-suggested improvement.

--- src/main/java/net/logicsquad/nanocaptcha/content/ArabicContentProducer.java
+++ src/main/java/net/logicsquad/nanocaptcha/content/ArabicContentProducer.java
@@ -11,7 +11,7 @@ public class ArabicContentProducer extends AbstractContentProducer implements Co
 	/**
 	 * Array containing some Arabic characters
 	 */
-	private static char[] ARABIC_CHARS = { '\u0627', '\u0628', '\u062a', '\u062b', '\u062c', '\u062d', '\u062e',
+	private static final char[] ARABIC_CHARS = { '\u0627', '\u0628', '\u062a', '\u062b', '\u062c', '\u062d', '\u062e',
 			'\u062f', '\u0630', '\u0631', '\u0632', '\u0633', '\u0634', '\u0635', '\u0636', '\u0637', '\u0638',
 			'\u0639', '\u063a', '\u0641', '\u0642', '\u0643', '\u0644', '\u0645', '\u0646', '\u0647', '\u0648',
 			'\u064a' };
