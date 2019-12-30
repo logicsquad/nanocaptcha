41c8744bf33d91c476b74bbba546a44bbf303819
Paul Hoadley <paulh@logicsquad.net>
2019-12-30 17:32:13 +1030

    Improves Javadocs and tidies up.

--- src/main/java/net/logicsquad/nanocaptcha/audio/Sample.java
+++ src/main/java/net/logicsquad/nanocaptcha/audio/Sample.java
@@ -1,6 +1,7 @@
 package net.logicsquad.nanocaptcha.audio;
 
 import java.io.*;
+import java.util.Objects;
 
 import javax.sound.sampled.*;
 
@@ -29,149 +30,182 @@ import javax.sound.sampled.*;
  * @since 1.0
  */
 public class Sample {
+	/**
+	 * {@link AudioFormat} for all {@code Sample}s
+	 */
+	public static final AudioFormat SC_AUDIO_FORMAT = new AudioFormat(16000, // sample rate
+			16, // sample size in bits
+			1, // channels
+			true, // signed?
+			false); // big endian?;
 
-    public static final AudioFormat SC_AUDIO_FORMAT = new AudioFormat(
-            16000, // sample rate
-            16, // sample size in bits
-            1, // channels
-            true, // signed?
-            false); // big endian?;
+	/**
+	 * {@link AudioInputStream} for this {@code Sample}
+	 */
+	private final AudioInputStream audioInputStream;
 
-    private final AudioInputStream _audioInputStream;
+	/**
+	 * Constructor taking a filename.
+	 * 
+	 * @param filename filename
+	 * @throws NullPointerException if {@code filename} is {@code null}
+	 */
+	public Sample(String filename) {
+		this(Sample.class.getResourceAsStream(Objects.requireNonNull(filename)));
+	}
 
-    public Sample(String filename) {
-		this(Sample.class.getResourceAsStream(filename));
-    }
+	/**
+	 * Constructor taking an {@link InputStream}.
+	 * 
+	 * @param is an {@link InputStream}
+	 * @throws NullPointerException if {@code is} is {@code null}
+	 */
+	public Sample(InputStream is) {
+		Objects.requireNonNull(is);
+		if (is instanceof AudioInputStream) {
+			audioInputStream = (AudioInputStream) is;
+			return;
+		}
 
-    public Sample(InputStream is) {
-        if (is instanceof AudioInputStream) {
-            _audioInputStream = (AudioInputStream) is;
-            return;
-        }
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(is);
 
-        try {
-            _audioInputStream = AudioSystem.getAudioInputStream(is);
+		} catch (UnsupportedAudioFileException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-        } catch (UnsupportedAudioFileException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+		checkFormat(audioInputStream.getFormat());
+	}
 
-        checkFormat(_audioInputStream.getFormat());
-    }
+	/**
+	 * Returns {@link AudioInputStream} for this {@code Sample}.
+	 * 
+	 * @return {@link AudioInputStream}
+	 */
+	public AudioInputStream getAudioInputStream() {
+		return audioInputStream;
+	}
 
-    public AudioInputStream getAudioInputStream() {
-        return _audioInputStream;
-    }
+	/**
+	 * Returns {@link AudioFormat} for this {@code Sample}.
+	 * 
+	 * @return {@link AudioFormat}
+	 */
+	public AudioFormat getFormat() {
+		return audioInputStream.getFormat();
+	}
 
-    public AudioFormat getFormat() {
-        return _audioInputStream.getFormat();
-    }
+	/**
+	 * Return the number of samples for all channels.
+	 * 
+	 * @return number of samples for all channels
+	 */
+	public long getSampleCount() {
+		long total = (audioInputStream.getFrameLength() * getFormat().getFrameSize() * 8)
+				/ getFormat().getSampleSizeInBits();
+		return total / getFormat().getChannels();
+	}
 
-    /**
-     * Return the number of samples of all channels
-     * 
-     * @return The number of samples for all channels
-     */
-    public long getSampleCount() {
-        long total = (_audioInputStream.getFrameLength()
-                * getFormat().getFrameSize() * 8)
-                / getFormat().getSampleSizeInBits();
-        return total / getFormat().getChannels();
-    }
+	/**
+	 * Returns interleaved samples for this {@code Sample}.
+	 * 
+	 * @return interleaved samples
+	 */
+	public double[] getInterleavedSamples() {
+		double[] samples = new double[(int) getSampleCount()];
+		try {
+			getInterleavedSamples(0, getSampleCount(), samples);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    public double[] getInterleavedSamples() {
-        double[] samples = new double[(int) getSampleCount()];
-        try {
-            getInterleavedSamples(0, getSampleCount(), samples);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		return samples;
+	}
 
-        return samples;
-    }
+	/**
+	 * Returns the interleaved decoded samples for all channels, from sample index
+	 * {@code start} (included) to sample index {@code end} (excluded) and copy them
+	 * into {@code samples}. {@code end} must not exceed {@code getSampleCount()},
+	 * and the number of samples must not be so large that the associated byte array
+	 * cannot be allocated.
+	 * 
+	 * @param start   start index
+	 * @param end     end index
+	 * @param samples destination array
+	 * @throws IOException              if unable to read from
+	 *                                  {@link AudioInputStream}
+	 * @throws IllegalArgumentException if sample is too large
+	 */
+	private double[] getInterleavedSamples(long start, long end, double[] samples)
+			throws IOException, IllegalArgumentException {
+		long nbSamples = end - start;
+		long nbBytes = nbSamples * (getFormat().getSampleSizeInBits() / 8) * getFormat().getChannels();
+		if (nbBytes > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Too many samples. Try using a smaller wav.");
+		}
+		// allocate a byte buffer
+		byte[] inBuffer = new byte[(int) nbBytes];
+		// read bytes from audio file
+		audioInputStream.read(inBuffer, 0, inBuffer.length);
+		// decode bytes into samples.
+		decodeBytes(inBuffer, samples);
 
-    /**
-     * Get the interleaved decoded samples for all channels, from sample index
-     * <code>begin</code> (included) to sample index <code>end</code> (excluded)
-     * and copy them into <code>samples</code>. <code>end</code> must not exceed
-     * <code>getSampleCount()</code>, and the number of samples must not be so
-     * large that the associated byte array cannot be allocated
-     * 
-     * @param begin
-     * @param end
-     * @param samples
-     * @throws IOException
-     * @throws IllegalArgumentException
-     */
-    public double[] getInterleavedSamples(long begin, long end, double[] samples)
-            throws IOException, IllegalArgumentException {
-        long nbSamples = end - begin;
-        long nbBytes = nbSamples * (getFormat().getSampleSizeInBits() / 8)
-                * getFormat().getChannels();
-        if (nbBytes > Integer.MAX_VALUE) {
-            throw new IllegalArgumentException(
-                    "Too many samples. Try using a smaller wav.");
-        }
-        // allocate a byte buffer
-        byte[] inBuffer = new byte[(int) nbBytes];
-        // read bytes from audio file
-        _audioInputStream.read(inBuffer, 0, inBuffer.length);
-        // decode bytes into samples.
-        decodeBytes(inBuffer, samples);
+		return samples;
+	}
 
-        return samples;
-    }
+	/**
+	 * Decodes audio as bytes in {@code audioBytes} into audio as samples and writes
+	 * the result into {@code audioSamples}.
+	 * 
+	 * @param audioBytes   source audio as bytes
+	 * @param audioSamples destination audio as samples
+	 */
+	private void decodeBytes(byte[] audioBytes, double[] audioSamples) {
+		int sampleSizeInBytes = getFormat().getSampleSizeInBits() / 8;
+		int[] sampleBytes = new int[sampleSizeInBytes];
+		int k = 0; // index in audioBytes
+		for (int i = 0; i < audioSamples.length; i++) {
+			// collect sample byte in big-endian order
+			if (getFormat().isBigEndian()) {
+				// bytes start with MSB
+				for (int j = 0; j < sampleSizeInBytes; j++) {
+					sampleBytes[j] = audioBytes[k++];
+				}
+			} else {
+				// bytes start with LSB
+				for (int j = sampleSizeInBytes - 1; j >= 0; j--) {
+					sampleBytes[j] = audioBytes[k++];
+					if (sampleBytes[j] != 0)
+						j = j + 0;
+				}
+			}
+			// get integer value from bytes
+			int ival = 0;
+			for (int j = 0; j < sampleSizeInBytes; j++) {
+				ival += sampleBytes[j];
+				if (j < sampleSizeInBytes - 1)
+					ival <<= 8;
+			}
+			// decode value
+			double ratio = Math.pow(2., getFormat().getSampleSizeInBits() - 1);
+			double val = ((double) ival) / ratio;
+			audioSamples[i] = val;
+		}
+	}
 
-    // Decode bytes of audioBytes into audioSamples
-    public void decodeBytes(byte[] audioBytes, double[] audioSamples) {
-        int sampleSizeInBytes = getFormat().getSampleSizeInBits() / 8;
-        int[] sampleBytes = new int[sampleSizeInBytes];
-        int k = 0; // index in audioBytes
-        for (int i = 0; i < audioSamples.length; i++) {
-            // collect sample byte in big-endian order
-            if (getFormat().isBigEndian()) {
-                // bytes start with MSB
-                for (int j = 0; j < sampleSizeInBytes; j++) {
-                    sampleBytes[j] = audioBytes[k++];
-                }
-            } else {
-                // bytes start with LSB
-                for (int j = sampleSizeInBytes - 1; j >= 0; j--) {
-                    sampleBytes[j] = audioBytes[k++];
-                    if (sampleBytes[j] != 0)
-                        j = j + 0;
-                }
-            }
-            // get integer value from bytes
-            int ival = 0;
-            for (int j = 0; j < sampleSizeInBytes; j++) {
-                ival += sampleBytes[j];
-                if (j < sampleSizeInBytes - 1)
-                    ival <<= 8;
-            }
-            // decode value
-            double ratio = Math.pow(2., getFormat().getSampleSizeInBits() - 1);
-            double val = ((double) ival) / ratio;
-            audioSamples[i] = val;
-        }
-    }
+	@Override
+	public String toString() {
+		return "[Sample] samples: " + getSampleCount() + ", format: " + getFormat();
+	}
 
-
-    @Override public String toString() {
-        return "[Sample] samples: " + getSampleCount() + ", format: "
-                + getFormat();
-    }
-
-    private static final void checkFormat(AudioFormat af) {
-        if (!af.matches(SC_AUDIO_FORMAT)) {
-            throw new IllegalArgumentException(
-                    "Unsupported audio format.\nReceived: " + af.toString()
-                            + "\nExpected: " + SC_AUDIO_FORMAT);
-
-        }
-    }
+	private static void checkFormat(AudioFormat af) {
+		if (!af.matches(SC_AUDIO_FORMAT)) {
+			throw new IllegalArgumentException(
+					"Unsupported audio format.\nReceived: " + af.toString() + "\nExpected: " + SC_AUDIO_FORMAT);
+		}
+	}
 }
