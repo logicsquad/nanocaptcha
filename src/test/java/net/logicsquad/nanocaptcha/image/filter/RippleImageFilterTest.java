adc1f1ad6109bc45c3ed8b226da4959489d67710
Paul Hoadley <paulh@logicsquad.net>
2021-02-14 15:54:23 +1030

    #4 Adds unit test for RippleImageFilter.

---
+++ src/test/java/net/logicsquad/nanocaptcha/image/filter/RippleImageFilterTest.java
@@ -0,0 +1,67 @@
+package net.logicsquad.nanocaptcha.image.filter;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests on {@link RippleImageFilter} class.
+ * 
+ * @author paulh
+ * @since 1.2
+ */
+public class RippleImageFilterTest {
+	private ImageFilter rippleImageFilter;
+
+	@Before
+	public void setup() {
+		rippleImageFilter = new RippleImageFilter();
+		return;
+	}
+
+	/**
+	 * Compares a known expected result to an actual transformation. This method is
+	 * to allow us to make changes to the implementation of
+	 * {@link RippleImageFilter} and confirm its functionality is unchanged.
+	 * 
+	 * @throws IOException if there is a problem reading images
+	 */
+	@Test
+	public void rippleImageFilterProducesExpectedTransformation() throws IOException {
+		BufferedImage input = ImageIO.read(getClass().getClassLoader().getResourceAsStream("input.png"));
+		BufferedImage expected = ImageIO.read(getClass().getClassLoader().getResourceAsStream("output.png"));
+		rippleImageFilter.filter(input);
+		assertTrue(bufferedImagesEqual(expected, input));
+		return;
+	}
+
+	/**
+	 * 
+	 * @param expected expected {@link BufferedImage}
+	 * @param actual   actual {@link BufferedImage}
+	 * @return {@code true} if {@code expected} and {@code actual} are the same,
+	 *         otherwise {@code false}
+	 * @see <a href=
+	 *      "https://stackoverflow.com/questions/15305037/java-compare-one-bufferedimage-to-another">Stack
+	 *      Overflow</a>
+	 */
+	private boolean bufferedImagesEqual(BufferedImage expected, BufferedImage actual) {
+		if (expected.getWidth() == actual.getWidth() && expected.getHeight() == actual.getHeight()) {
+			for (int x = 0; x < expected.getWidth(); x++) {
+				for (int y = 0; y < expected.getHeight(); y++) {
+					if (expected.getRGB(x, y) != actual.getRGB(x, y))
+						return false;
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
+	}
+}
